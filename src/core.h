af9f6de2653bea8148d016a87cea0046b6b876c4
Abhishek Chakravarti <abhishek@taranjali.org>
2019-09-08 12:24:13 +0530

    document interface of hint module
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/core.h
+++ src/core.h
@@ -1,6 +1,35 @@
 #if !defined ARGENT_CORE
 #define ARGENT_CORE
 
+
+/**************************************************************************//**
+ * @defgroup hint Argent Core Hint Module
+ * Compiler hints for optimisation.
+ *
+ * Compilers such as GCC have always focused aggressively on optimisation. Many
+ * such optimisations have been introduced as compiler-specific extensions that
+ * are @b not part of the C standards.
+ *
+ * The Hint Module abstracts such optimisations as conditional macros so that
+ * they they can be taken advantage of in GCC and GCC-compatible compilers such
+ * as Clang, but at the same time degrade safely on other compilers.
+ * @{
+ */
+
+
+/**
+ * Hints that a function is pure.
+ *
+ * The @c arc_pure macro is used to decorate the declaration of a pure function.
+ * A pure function is one that always returns the same value for the same
+ * arguments without any side effects. This hint follows the GCC attribute
+ * syntax, and is recommended to be placed in the function declaration just
+ * before the function return type.
+ *
+ * @warning This hint is available only on GCC and GCC-compatible compilers such
+ * as Clang; on other compilers, this macro degrades with a warning that it has
+ * no effect.
+ */
 #if (defined __GNUC__ || defined __clang__)
 #   define arc_pure __attribute__((pure))
 #else
@@ -8,6 +37,21 @@
 #   warning arc_pure has no effect on non GCC-compatible compilers
 #endif
 
+
+/**
+ * Hints that a function is called frequently.
+ *
+ * The @c arc_hot macro is used to decorate a function declaration in order to
+ * indicate that it is called frequently. This hint follows the GCC attribute
+ * syntax, and is recommended to be placed in the function declaration just
+ * before the function return type.
+ *
+ * @warning This hint is available only on GCC and GCC-compatible compilers such
+ * as Clang; on other compilers, this macro degrades with a warning that it has
+ * no effect.
+ *
+ * @see arc_cold
+ */
 #if (defined __GNUC__ || defined __clang__)
 #   define arc_hot __attribute__((hot))
 #else
@@ -16,6 +60,20 @@
 #endif
 
 
+/**
+ * Hints that a function is called infrequently.
+ *
+ * The @c arc_cold macro is used to decorate a function declaration in order to
+ * indicate that is called infrequently. This hint follows the GCC attribute
+ * syntax, and is recommended to be placed in the function declaration just
+ * before the function return type.
+ *
+ * @warning This hint is available only on GCC and GCC-compatible compilers such
+ * as Clang; on other compilers, this macro degrades with a warning that it has
+ * no effect.
+ *
+ * @see arc_hot
+ */
 #if (defined __GNUC__ || defined __clang__)
 #   define arc_cold __attribute__((cold))
 #else
@@ -24,6 +82,21 @@
 #endif
 
 
+/**
+ * Hints that a predicate is likely to be true.
+ *
+ * The @c arc_likely() macro indicates that a predicate expression @p p is @b
+ * likely to evaluate to @c true. This hint has been modelled after the @c
+ * likely() macro used in the Linux kernel source.
+ *
+ * @param p predicate expected to be true.
+ *
+ * @warning This hint is available only on GCC and GCC-compatible compilers such
+ * as Clang; on other compilers, this macro degrades with a warning that it has
+ * no effect.
+ *
+ * @see arc_unlikely()
+ */
 #if (defined __GNUC__ || defined __clang__)
 #   define arc_likely(p) (__builtin_expect(!!(p), 1))
 #else
@@ -32,6 +105,21 @@
 #endif
 
 
+/**
+ * Hints that a predicate is likely to be false.
+ *
+ * The @c arc_unlikely() macro indicates that a predicate expression @p p is @b
+ * unlikely to evaluate to @c false. This hint has been modelled after the @c
+ * unlikely() macro used in the Linux kernel source.
+ *
+ * @param p predicate expected to be false.
+ *
+ * @warning This hint is available only on GCC and GCC-compatible compilers such
+ * as Clang; on other compilers, this macro degrades with a warning that it has
+ * no effect.
+ *
+ * @see arc_likely()
+ */
 #if (defined __GNUC__ || defined __clang__)
 #   define arc_unlikely(p) (__builtin_expect(!!(p), 0))
 #else
