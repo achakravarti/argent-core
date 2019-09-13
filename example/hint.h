94143ec93029c9504f2168d37e0db637c3100dea
Abhishek Chakravarti <abhishek@taranjali.org>
2019-09-13 10:09:21 +0530

    Simplify Argent namespace
    Experience has shown that it makes sense to use a common namespace for
    the Argent Libraries rather than specific ones for each. Accordingly,
    the `agc_` namespace used earlier has been renamed to `ag_`. The
    namespace in the other Argent Libraries need to be modified accordingly.
    
    As a result of this simplification, it is now possible to define the
    `ag_string` type within the Types Module of the Core Library.
    
    The examples have also been updated accordingly, and a new example has
    been added for `ag_string`.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- example/hint.h
+++ example/hint.h
@@ -1,42 +1,43 @@
+#include <stdio.h>
 #include <argent/core.h>
 
 
-/* This is how you would declare a hot function. */
-extern agc_hot void
+    /* this is how you would declare a hot function */
+extern ag_hot void
 foo_hot(int x);
 
 
-/* This is how you would declare a cold function. */
-static agc_cold void
+    /* this is how you would declare a cold function */
+static ag_cold void
 bar_cold(int y);
 
 
-/* This is how you would declare a pure function. Note that foo_hot() and
- * bar_cold() could also have been declared as pure. */
-extern agc_pure int
+    /* this is how you would declare a pure function; note that foo_hot() and
+     * bar_cold() could also have been declared as pure */
+extern ag_pure int
 foobar_pure(int x, int y);
 
 
-/* The hot function shows how you would indicate a likely predicate. */
+    /* this hot function shows how you would indicate a likely predicate */
 void
 foo_hot(int x)
 {
-    if (agc_likely (x != 0))
+    if (ag_likely (x != 0))
         printf("x = %d\n", x);
 }
 
 
-/* The cold function shows how you would indicate an unlikely predicate. */
+    /* this cold function shows how you would indicate an unlikely predicate */
 void
 bar_cold(int y)
 {
-    if (agc_unlikely (y > 1))
+    if (ag_unlikely (y > 1))
         printf("y = %d\n", y);
 }
 
 
-/* foobar_pure() shows how it qualifies for being declared pure by returning the
- * same value for the same arguments without any side effects. */
+    /* foobar_pure() shows how it qualifies for being declared pure by returning
+     * the same value for the same arguments without any side effects */
 int
 foobar_pure(int x, int y)
 {
--- example/type.h
+++ example/type.h
@@ -2,103 +2,114 @@
 #include <argent/core.h>
 
 
-    /* this function gives an example of how to use the agc_bool type */
+    /* this function gives an example of how to use the ag_bool type */
 static void
 bool_example(void)
 {
-    agc_bool b = AGC_BOOL_TRUE;
+    ag_bool b = AGC_BOOL_TRUE;
     printf ("The value of b is %d\n", b);
 }
 
 
-    /* this function gives examples of how to use the agc_word types */
+    /* this function gives examples of how to use the ag_word types */
 static void
 word_example(void)
 {
-    agc_word w = 0;
+    ag_word w = 0;
     printf ("The value of w is %lu\n", w);
 
-    agc_word_8 w8 = 1;
+    ag_word_8 w8 = 1;
     printf ("The value of w8 is %u\n", w8);
 
-    agc_word_16 w16 = 5;
+    ag_word_16 w16 = 5;
     printf ("The value of w16 is %u\n", w16);
 
-    agc_word_32 w32 = 105;
+    ag_word_32 w32 = 105;
     printf ("The value of w32 is %u\n", w32);
 
-    agc_word_64 w64 = 100005;
+    ag_word_64 w64 = 100005;
     printf ("The value of w64 is %u\n", w64);
 }
 
 
-    /* this function gives examples of how to use the agc_size and agc_index
+    /* this function gives examples of how to use the ag_size and ag_index
      * types */
 static void
 size_example(void)
 {
-    agc_size s = sizeof (agc_int);
+    ag_size s = sizeof (ag_int);
     printf ("The value of s is %lu\n", s);
 
-    agc_index i = 5;
+    ag_index i = 5;
     printf ("The value of i is %lu\n", i);
 }
 
 
-    /* this function gives examples of how to use the agc_uint types */
+    /* this function gives examples of how to use the ag_uint types */
 static void
 uint_example(void)
 {
-    agc_uint u = 0;
+    ag_uint u = 0;
     printf ("The value of u is %lu\n", u);
 
-    agc_uint_8 u8 = 1;
+    ag_uint_8 u8 = 1;
     printf ("The value of u8 is %u\n", u8);
 
-    agc_uint_16 u16 = 5;
+    ag_uint_16 u16 = 5;
     printf ("The value of u16 is %u\n", u16);
 
-    agc_uint_32 u32 = 105;
+    ag_uint_32 u32 = 105;
     printf ("The value of u32 is %u\n", u32);
 
-    agc_uint_64 u64 = 100005;
+    ag_uint_64 u64 = 100005;
     printf ("The value of u64 is %u\n", u64);
 }
 
 
-    /* this function gives examples of how to use the agc_int types */
+    /* this function gives examples of how to use the ag_int types */
 static void
 int_example(void)
 {
 
-    agc_int i = 0;
+    ag_int i = 0;
     printf ("The value of i is %lu\n", i);
 
-    agc_int_8 i8 = 1;
+    ag_int_8 i8 = 1;
     printf ("The value of i8 is %u\n", i8);
 
-    agc_int_16 i16 = 5;
+    ag_int_16 i16 = 5;
     printf ("The value of i16 is %u\n", i16);
 
-    agc_int_32 i32 = -105;
+    ag_int_32 i32 = -105;
     printf ("The value of i32 is %u\n", i32);
 
-    agc_int_64 i64 = 100005;
+    ag_int_64 i64 = 100005;
     printf ("The value of i64 is %lu\n", i64);
 }
 
 
-    /* this function gives examples of how to use the agc_float types */
+    /* this function gives examples of how to use the ag_float types */
 static void
 float_example(void)
 {
-    agc_float f = 3.14;
+    ag_float f = 3.14;
     printf ("The value of f is %lf\n", f);
 
-    agc_float_32 f32 = 3.14;
+    ag_float_32 f32 = 3.14;
     printf ("The value of f32 is %f\n", f32);
 
-    agc_float_64 f64 = 3.14;
+    ag_float_64 f64 = 3.14;
     printf ("The value of f64 is %f\n", f64);
 }
+
+
+    /* this function gives an example of how to use the ag_string type; see the
+     * Argent Abstractions Library for more examples */
+static void
+string_example(void)
+{
+    const ag_string *s = "Hello, world!";
+    printf ("%s\n", s);
+}
+
 
